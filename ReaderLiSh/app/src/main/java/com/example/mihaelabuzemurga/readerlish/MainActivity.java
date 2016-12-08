5ef62799a6ae8525a1abf0aadbb4994fdfb12b8e
MihaelaBuzemurga <mihaelabuzemurga@gmail.com>
2016-12-08 19:37:06 +0200

    alte_pg_design

--- ReaderLiSh/app/src/main/java/com/example/mihaelabuzemurga/readerlish/MainActivity.java
+++ ReaderLiSh/app/src/main/java/com/example/mihaelabuzemurga/readerlish/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        //setContentView(R.layout.login)
     }
     public void ConnectToServer() {
         System.out.println("rrrrrrrrrrrrrrrrrr");
